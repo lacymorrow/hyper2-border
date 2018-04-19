b2b35535b009a12ecfc7b29f0434b1a1f200bdd9
Lacy Morrow <me@lacymorrow.com>
2018-04-18 17:03:48 -0700

    init hyper-border v2

--- README.md
+++ README.md
@@ -1,38 +1,32 @@
 # hyperborder - extension for Hyper
 adds a gradient border to the `Hyper` terminal
 
-![](https://cldup.com/pL94ODfQNP.png)
+[![hyper-border](https://raw.githubusercontent.com/lacymorrow/hyper-border/screenshot.png)](https://github.com/lacymorrow/hyper-border)
 
 ## Installation
-add it to plugins in your `~/.hyper.js` configuration
+
+Add `hyper-border` to plugins in your `~/.hyper.js` configuration
 
 ````
 module.exports = {
   ...
-  plugins: ['hyperborder']
+  plugins: ['hyper-border']
   ...
 }
 ````
-then just restart `Hyper` app or go to the menu 'Plugins / Update All Now'
+
+then restart the `Hyper` app or go to the menu 'Plugins / Update All Now'
 
 ## Configuration
+
 The following settings can be configured by adding a `hyperBorder` section in your `.hyper.js` `config` section:
 
 | Setting              | Type                 | Description                                            |
 |----------------------|----------------------|--------------------------------------------------------|
 | `borderWidth`        | `string`             | CSS string for how thick the borders should be         |
 | `borderColors`       | `string`, `string[]` | The color(s) for the border                            |
-| `adminBorderColors`  | `string`, `string[]` | The color(s) for the border for an admin/elevated window. This follows the precedence  of `adminBorderColors` > `borderColors` > defaultColors                                    |
-| `blurredColors`      | `string`, `string[]` | The color(s) of the borders when the window isn't active |
-| `blurredAdminColors` | `string`, `string[]` | The color(s) of the borders when the admin/elevated window isn't active. This follows the precedence of `blurredAdminColors` > `blurredColors` > `adminBorderColors` > `borderColors` > defaultColors |
 
-## A note on admin/root colors
-The use of Hyper under the admin/root account is mainly intended for Windows' users (where it is common to run an application in
-elevated mode), since on Linux/OSX you would typically utilize the `sudo <command>` command. _Technically_ you can run Hyper as root
-on non-Windows machines (there are issues running Hyper as root under a [Wayland](https://wayland.freedesktop.org/) desktop), though
-in this case, the root user will actually have their own copy of `.hyper.js` configuration.
-
-### EXAMPLE: Set Border Colors And Width
+###### EXAMPLE: Set Border Colors And Width
 
 ```javascript
 module.exports = {
@@ -47,7 +41,7 @@ module.exports = {
 }
 ```
 
-### EXAMPLE: Set Border Colors To Random Colors
+###### EXAMPLE: Set Border Colors To Random Colors
 
 In addition, you can set any color value to `'random'` (string value):
 
@@ -66,54 +60,4 @@ module.exports = {
 
 Then every newly opened `Hyper` terminal window will have a different colored border.
 
-### EXAMPLE: Animate Border Colors
-You like some animations? Then try this:
-
-```javascript
-module.exports = {
-  config: {
-    ...
-    hyperBorder: {
-      animate: true,
-      ...
-    }
-    ...
-  }
-}
-```
-
-To change the speed of animation, specify an object with a `duration` property:
-
-```javascript
-module.exports = {
-  config: {
-    ...
-    hyperBorder: {
-      animate: {
-        duration: '1s',  // default is 16s
-      },
-      ...
-    }
-    ...
-  }
-}
-```
-
-### EXAMPLE: Angled Gradients
-Because we use CSS3's `linear-gradient`, we're able to specify angles at which to create the radius. Set your own angle like this:
-
-```javascript
-module.exports = {
-  config: {
-    ...
-    hyperBorder: {
-      borderAngle: '180deg',
-      ...
-    }
-    ...
-  }
-}
-```
-
-## Download Hyper here
-https://hyper.is/
+# [Get Hyper](https://hyper.is/)
--- index.js
+++ index.js
@@ -1,122 +1,62 @@
-const {remote} = require('electron');
-const isElevated = require('is-elevated');
-const {createAnimator} = require('./lib/animator');
-const {getBorderColors} = require('./lib/colorhelpers');
+const { getBorderColors } = require('./lib/colors')
 
-let unloadAnimator = null;
+exports.decorateConfig = config => {
+  const defaultColors = ["#fc1da7", "#fba506"]
 
-module.exports.onRendererWindow = async window => {
-  const browserWindow = remote.getCurrentWindow();
-
-  browserWindow.on('blur', () => window.document.documentElement.classList.add('blurred'));
-  browserWindow.on('focus', () => window.document.documentElement.classList.remove('blurred'));
-
-  if (!browserWindow.isFocused()) {
-    window.document.documentElement.classList.add('blurred');
-  }
-
-  if (await isElevated()) {
-    window.document.documentElement.classList.add('elevated');
-  }
-
-  const config = window.config.getConfig();
-
-  if (config.hyperBorder && config.hyperBorder.animate) {
-    unloadAnimator = createAnimator(window, browserWindow);
-  }
-};
-
-module.exports.onUnload = async () => {
-  if (unloadAnimator) {
-    unloadAnimator();
-  }
-};
-
-module.exports.decorateConfig = config => {
-  const defaultColors = ['#fc1da7', '#fba506'];
-
-  const configObj = Object.assign({
-    animate: false,
-    borderWidth: '4px',
-    borderColors: defaultColors,
-    adminBorderColors: (config.hyperBorder && config.hyperBorder.borderColors) || defaultColors,
-    blurredAdminColors: (config.hyperBorder && (config.hyperBorder.blurredColors || config.hyperBorder.adminBorderColors)) || defaultColors,
-    blurredColors: defaultColors,
-    borderAngle: '180deg'
-  }, config.hyperBorder);
+  const configObj = Object.assign(
+    {
+      borderWidth: "4px",
+      borderColors: defaultColors,
+    },
+    config.hyperBorder
+  );
 
   return Object.assign({}, config, {
     css: `
-      html {
-        --border-width: ${configObj.borderWidth};
-        --border-angle: ${configObj.animate ? '269deg' : configObj.borderAngle};
-        --background-color: ${config.backgroundColor || '#000'};
-        --border-color: ${config.borderColor};
-        --border-colors: ${getBorderColors(configObj.borderColors).join(',')};
-        --admin-colors: ${getBorderColors(configObj.adminBorderColors).join(',')};
-        --blurred-colors: ${getBorderColors(configObj.blurredColors).join(',')};
-        --blurred-admin-colors: ${getBorderColors(configObj.blurredAdminColors).join(',')};
-      }
-      html {
-        height: 100%;
-        background: linear-gradient(var(--border-angle), var(--border-colors));
-        border-radius: var(--border-width);
-        overflow: hidden;
-      }
-      html.elevated {
-        background: linear-gradient(var(--border-angle), var(--admin-colors));
-      }
-      html.blurred {
-        background: linear-gradient(var(--border-angle), var(--blurred-colors));
-      }
-      html.blurred.elevated {
-        background: linear-gradient(var(--border-angle), var(--blurred-admin-colors));
-      }
-      body {
-        position: absolute;
-        top: var(--border-width);
-        bottom: var(--border-width);
-        left: var(--border-width);
-        right: var(--border-width);
-        border-radius: var(--border-width);
-      }
-      ${config.css || ''}
-      #mount {
-      }
-      .hyper_main {
-        background-color: var(--background-color);
-        top: var(--border-width);
-        bottom: var(--border-width);
-        left: var(--border-width);
-        right: var(--border-width);
-        border-width: 0px;
-      }
-      .hyper_main .header_header {
-        top: var(--border-width);
-        left: var(--border-width);
-        right: var(--border-width);
-      }
-      .hyper_main .tabs_list {
-        border-bottom-color: var(--border-color);
-        border-top-left-radius: var(--border-width);
-        border-top-right-radius: var(--border-width);
-      }
-      .hyper_main .tab_tab:last-child {
-        border-top-right-radius: var(--border-width);
-      }
-      .hyper_main .terms_terms {
-        border-radius: 0 0 var(--border-width) var(--border-width);
-        bottom: var(--border-width);
-        left: var(--border-width);
-        right: var(--border-width);
-      }
-      .hyper_main .terms_term {
-        margin-top: var(--border-width);
-      }
-      .header_windowHeaderWithBorder {
-        left: var(--border-width);
-        width: calc(100% - var(--border-width) - var(--border-width));
-      }
-    `
+        ${config.css || ""}
+
+        .hyper_main {
+          --border-angle: 180deg;
+          --border-width: ${configObj.borderWidth};
+          --background-color: ${config.backgroundColor || "#000"};
+          --border-color: ${config.borderColor};
+          --border-colors: ${getBorderColors(configObj.borderColors).join(",")};
+        }        
+        .hyper_main {
+          padding: var(--border-width);
+          background: linear-gradient(var(--border-angle), var(--border-colors));;
+        }
+        .hyper_main .header_header {
+          background-color: var(--background-color);
+          top: var(--border-width);
+          left: var(--border-width);
+          right: var(--border-width);
+        }
+        .hyper_main .terms_terms {
+          background-color: var(--background-color);
+          left: var(--border-width);
+          right: var(--border-width);
+          bottom: var(--border-width);
+        }
+
+        .hyper_main .tabs_list {
+          border-bottom-color: var(--border-color);
+          border-top-left-radius: var(--border-width);
+          border-top-right-radius: var(--border-width);
+        }
+        .hyper_main .tab_tab:last-child {
+          border-top-right-radius: var(--border-width);
+        }
+        .hyper_main .terms_terms {
+          border-radius: 0 0 var(--border-width) var(--border-width);
+          bottom: var(--border-width);
+          left: var(--border-width);
+          right: var(--border-width);
+        }
+        .header_windowHeaderWithBorder {
+          left: var(--border-width);
+          width: calc(100% - var(--border-width) - var(--border-width));
+        }
+      `
   });
 };
